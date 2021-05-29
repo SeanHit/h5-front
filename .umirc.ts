1488f9cc5f486badb1284b712877275390b8c1bc
刘祥 <lx_myall@163.com>
2021-05-29 13:42:56 +0800

    feat: umi路由配置

---
+++ .umirc.ts
@@ -0,0 +1,90 @@
+import path from 'path';
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  dynamicImport: {
+    loading: '@/components/LoadingCp',
+  },
+  dva: {
+    immer: true,
+  },
+  devtool: 'source-map',
+  antd: {},
+  title: 'H5页面编辑',
+  base: '/',
+  publicPath: '/',
+  outputPath: 'dist',
+  esbuild: {},
+  routes: [
+    {
+      exact: false,
+      path: '/',
+      component: '@/layouts/index',
+      routes: [
+        {
+          path: '/editor',
+          component: '../pages/editor',
+        },
+
+        {
+          path: '/login',
+          component: '../pages/login',
+        },
+        {
+          path: '/preview',
+          component: '../pages/editor/preview',
+        },
+        {
+          path: '/check',
+          component: '../pages/editor/preview',
+        },
+
+        {
+          path: '/backManagement/roles',
+          component: '../pages/backManagement/roles'
+        },
+        {
+          path: '/backManagement/privileges',
+          component: '../pages/backManagement/privileges'
+        },
+        {
+          path: '/',
+          component: '../pages/admin',
+          routes: [
+            {
+              /** 制作提示页面 */
+              path: '/',
+              redirect: '/h5/editor'
+            },
+            {
+              /** 制作提示页面 */
+              path: '/h5/editor',
+              component: '../pages/home',
+            },
+            {
+              path: '/h5/admin',
+              component: '../pages/h5Admin',
+            },
+            {
+              path: '/logs',
+              component: '../pages/logs'
+            },
+            {
+              path: '/backManagement/users',
+              component: '../pages/backManagement/users'
+            },
+          ]
+        },
+      ],
+    },
+  ],
+  theme: {
+    'primary-color': '#2F54EB',
+  },
+  extraBabelPlugins: [['import', { libraryName: 'zarm', style: true }]],
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+    utils: path.resolve(__dirname, 'src/utils/'),
+    assets: path.resolve(__dirname, 'src/assets/'),
+  },
+});
